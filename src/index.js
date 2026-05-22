b731cb693be2e85c0dc352715f1eba4aef74f3df
rohitthackermfc-prog <rohitthackermfc@gmail.com>
2026-05-22 18:57:34 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from  "react";
+import ReactDOM from  "react-dom/
+  client";
+import  App  From  "./App";
+
+const root  =
+ReactDom .createRoot (document. getElementById("root"));
+root. render (
+  <React .StrictMode>
+    <App />
+  </React. StrictMode>
+);
